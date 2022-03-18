da6f1b34130ee5d0c6d6af9bd44432c73aa7788a
Daniellimap402 <daniellimap402@gmail.com>
2022-03-18 20:12:36 -0300

    adiciona projeto

---
+++ porteiro/src/test/java/com/example/porteiro/PorteiroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.porteiro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PorteiroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
